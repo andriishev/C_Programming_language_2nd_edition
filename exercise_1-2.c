633ece23682506765e9d844aeef480563a05491e
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-25 13:14:04 -0800

    Add files via upload

---
+++ exercise_1-2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>              /* include information about standart I/O library */
+
+int main()                      
+{
+  printf("hello, world\c\n");   /* calling function printf to print sequence of characters */ 
+}                               /* and adding additional '\c' single character             */              
