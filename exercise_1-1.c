32f83fa10747b752b3119532051df31555a0b6e2
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-25 12:43:47 -0800

    Update exercise_1-1.c

--- exercise_1-1.c
+++ exercise_1-1.c
@@ -1,8 +1,6 @@
-#include <stdio.h>					/* include information about standart library */
+#include <stdio.h>				/* include information about standart I/O library */
 
 int main()											
 {
-
 	printf("hello, world\n");		/* calling function printf to print sequence of characters */
-
 }
