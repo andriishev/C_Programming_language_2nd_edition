57ecbfa1e78e64ddfb8fd795204d06a680186e23
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-25 12:42:45 -0800

    Add files via upload

---
+++ exercise_1-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>					/* include information about standart library */
+
+int main()											
+{
+
+	printf("hello, world\n");		/* calling function printf to print sequence of characters */
+
+}
