cba7035152b5735339e10b324ad6026987d4832e
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-26 23:48:26 -0800

    Add files via upload

---
+++ exercise_1-9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int c, bl;
+
+	bl = 0;			/* blank counter */
+
+	while((c = getchar()) != EOF){
+		if(c == ' ' && bl == 0){
+			putchar(' ');
+				++bl;
+		}
+ 		else if (c != ' '){
+			putchar(c);
+			bl = 0;
+		}
+	}
+}
