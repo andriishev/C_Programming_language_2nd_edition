6766cae8cbc814fc0385ceb9e0d05fdc0567160f
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-26 22:25:39 -0800

    Add files via upload

---
+++ exercise_1-5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* print Fahrenheit - Celsius table */
+
+#define UPPER 300 // upper limit of table 
+#define LOWER 0   // lower limit of table
+#define STEP 20   // step size 
+
+int main()
+{
+	int fahr;
+
+	for(fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
+		printf("%3d\t%6.1f\n",fahr, (5.0/9.0)*(fahr - 32));
+	}
+}
