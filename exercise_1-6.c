32ae15a7df9d27d72358da57942032eefddfd78a
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-27 20:00:27 -0800

    Update exercise_1-6.c

--- exercise_1-6.c
+++ exercise_1-6.c
@@ -2,6 +2,5 @@
 
 int main()
 {
-	printf("%d\n", getchar() != EOF);	/* printing value of getchar() != EOF expression */
-	
+	printf("%d\n", getchar() != EOF);	/* printing value of getchar() != EOF expression */	
 }
