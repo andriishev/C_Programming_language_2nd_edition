503b13e88a5c25fac327de3fa6e791cd1cddf64d
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-27 19:59:28 -0800

    Update exercise_1-2.c

--- exercise_1-2.c
+++ exercise_1-2.c
@@ -2,5 +2,5 @@
 
 int main()                      
 {
-	printf("hello, world\c\n");   /* calling function printf to print sequence of characters */ 
-}                               /* and adding additional '\c' single character             */              
+	printf("hello, world\c\n");   	/* calling function printf to print sequence of characters */ 
+}                               	/* and adding additional '\c' single character */              
