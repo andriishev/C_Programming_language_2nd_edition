fd1336882215358a127c78002c1984c4e2304236
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2020-12-27 19:58:40 -0800

    Delete exercise_1-2.c

--- exercise_1-2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>              /* include information about standard I/O library */
-
-int main()                      
-{
-  printf("hello, world\c\n");   /* calling function printf to print sequence of characters */ 
-}                               /* and adding additional '\c' single character */              
