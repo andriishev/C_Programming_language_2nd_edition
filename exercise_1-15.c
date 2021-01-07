85d7c09b443f8451508cc8ce3ae381f44b0013c5
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2021-01-06 19:01:12 -0800

    Add files via upload

---
+++ exercise_1-15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* print Fahrenheit - Celsius table */
+
+float temp_conver(float fahr);
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0; 					/* lower limt of temperature */
+	upper = 300; 				/* upper limit 				 */
+	step = 20;					/* step size 				 */
+	
+	fahr = lower;
+
+	printf("Fahr\tCelsius Table\n");		/* printing  a heading above the table */
+	
+	while(fahr <= upper){
+		printf("%3.0f\t%5.1f\n", temp_conver(fahr));
+		fahr += step;
+	}
+}
+
+/* converting Fahrenheit to Celsius */
+
+float temp_conver(float fahr)
+{
+	float celsius;
+
+	celsius = (5.0/9.0) * (fahr - 32.0);
+
+	return celsius;
+}
