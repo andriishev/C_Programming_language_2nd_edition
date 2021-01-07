06f79571469567065d205c902878e2214e8baea8
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2021-01-06 19:06:57 -0800

    Add files via upload

---
+++ exercise_1-16.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define MAXLINE 1000	/* maximum input line size */
+
+int get_line(char s[], int max_line);
+void copy(char to[], char from[]);
+
+int main()
+{
+	int len;				/* current line length */
+	int max;				/* maximum lenght seen so far */
+	char line[MAXLINE];		/* current input line */
+	char longest[MAXLINE];	/* longest line saved */
+	
+	max = 0;
+	while ((len = get_line(line, MAXLINE)) > 0){
+		printf("%d, %s",len, line);
+		if (len > max){
+			max = len;
+			copy(longest, line);
+		}
+	}
+	if (max > 0)  			/* there was a line */
+		printf("Longest line :%s\n", longest);
+	return 0;
+
+}
+
+/* getline - read the line into s, return length */
+
+int get_line(char s[], int max_line)
+{
+	int c, i;
+
+	for (i = 0; i <max_line - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy 'from' into 'to'; assume to is big enough */
+
+void copy(char to[], char from[])
+{
+	int i;
+
+	i = 0;
+	while((to[i] = from[i]) != '\0')
+		i++;
+}
---
+++ exercise_1-17.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAXLINE 1000	/* maximum input line size */
+#define MINLINE 80		/* minimum input line size */
+
+int get_line(char s[], int max_line);
+
+
+int main()
+{
+	int len;				/* current line length */
+	char line[MAXLINE];		/* current input line */
+
+	while ((len = get_line(line, MAXLINE)) > 0)
+		if (len > MINLINE) 
+			printf("your input line is: %s\n", line);
+		else
+			printf("your input line is too short\n");
+	
+	return 0;
+
+}
+
+/* getline - read the line into s, return length */
+
+int get_line(char s[], int max_line)
+{
+	int c, i;
+
+	for (i = 0; i <max_line - 1 && (c = getchar()) != EOF && c != '\n'; ++i){
+		s[i] = c;
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
