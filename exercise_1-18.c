0fc080c87a89f4944b8b11c162bc1a31278c8543
Andrii Shevchenko <54296911+andriishev@users.noreply.github.com>
2021-01-07 18:11:33 -0800

    Add files via upload

---
+++ exercise_1-18.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAXLINE 10
+#define MINLINE 2
+
+int get_line(char s[], int max_line);
+void f_remove(char c[]);
+
+
+int main()
+{
+	int len;					/* current line length */
+	char line[MAXLINE];			/* current input line */
+
+	while ((len = get_line(line, MAXLINE)) > 0)
+		if (len >= MINLINE){
+			f_remove(line);
+			printf("your input line is: %s", line);
+		}
+		else
+			printf("your input line is too short\n");
+	
+	return 0;
+}
+
+/* getline - read the line into s, return length */
+
+int get_line(char s[], int max_line)
+{
+	int c, i;
+
+	for (i = 0; i < max_line - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		s[i] = c;
+	if (c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* function to remove trailing blanks and tabs from each input */
+void f_remove(char c[])
+{	
+	int i = 0;
+
+	while(c[i] != '\n')
+		i++;
+	i--;
+	while(i >= 0 && (c[i] == ' ' || c[i] == '\t'))
+		i--;
+	if(i >= -1){
+		i++;
+		c[i] = '\n';
+		i++;
+		c[i] = '\0';
+	}
+}
